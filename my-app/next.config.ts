6bbefb6542ae9b0c8637aea2145ac4f8fee2057f
VEDANTtheDev <vedantnarlawar25@gmail.com>
2025-01-09 00:57:14 +0530

    Added basic Next.js configuration

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true, // Enables React Strict Mode
+  swcMinify: true, // Enables SWC-based minification
 };
 
 export default nextConfig;
